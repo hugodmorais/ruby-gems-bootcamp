baea7cd29e49df574c62adf876a754f977b89743
hugodmorais <hugodanielmorais@gmail.com>
2021-05-26 22:07:09 +0000

    Add devise gem, set relation(user/course) and add header

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  devise_for :users
   resources :courses
-  root 'home#index'
+  root to: 'home#index'
 
   #get 'static_pages/privacy_policy'
   get 'privacy_policy', to: 'static_pages#privacy_policy'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,11 @@
+# Create User
+User.create!(email: 'admin@admin.com', password: '123456', password_confirmation: '123456')
+
+# Create Courses
 30.times do
   Course.create!([{
     title: Faker::Educator.course_name,
-    description: Faker::TvShows::GameOfThrones.quote
+    description: Faker::TvShows::GameOfThrones.quote,
+    user_id: User.first.id
   }])
 end
